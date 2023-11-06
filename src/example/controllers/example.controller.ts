3e28d28b5d887464f1ef6b6599583a92be4ab176
devoren22 <orenkaizer.dev@gmail.com>
2023-11-06 21:16:33 +0200

    add middleware demo example

--- src/example/controllers/example.controller.ts
+++ src/example/controllers/example.controller.ts
@@ -1,7 +1,33 @@
 import { Service } from "typedi";
-import { Body, Controller, Get, Param, Post } from "../../express";
+import { Body, Controller, Get, Middlewares, Param, Post, Req } from "../../express";
 import { ExampleService } from "../services/example.service";
 import { AppConfiguration, ConfigService } from "../../configuration/configuration";
+import { IMiddleware } from "../../common/types";
+import { Request, Response, NextFunction } from "express";
+import { ParamsDictionary } from "express-serve-static-core";
+import { ParsedQs } from "qs";
+
+@Service()
+export class ExampleMiddleware1 implements IMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    console.log("enter middleware..");
+
+    req.body["middleware1"] = "example1";
+
+    next();
+  }
+}
+
+@Service()
+export class ExampleMiddleware2 implements IMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    console.log("enter middleware..");
+
+    req.body["middleware2"] = "example2";
+
+    next();
+  }
+}
 
 @Controller("/example")
 @Service()
@@ -19,13 +45,15 @@ export class ExampleController {
   }
 
   @Post("/check")
+  @Middlewares(ExampleMiddleware1, ExampleMiddleware2)
   async addCheck(
     @Body() body: { firstName: string; lastName: string },
     @Body("firstName") firstNameAgain: string,
-    @Body("lastName") lastNameAgain: string
+    @Body("lastName") lastNameAgain: string,
+    @Req() request: Request
   ) {
     const { firstName, lastName } = body;
 
-    return `${firstName} ${lastName} ${firstNameAgain} ${lastNameAgain}`;
+    return `${request.body["middleware1"]} ${request.body["middleware2"]} ${firstName} ${lastName} ${firstNameAgain} ${lastNameAgain}`;
   }
 }
